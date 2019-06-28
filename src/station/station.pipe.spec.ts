bed8a12110a2e54b47d1f18ed0f586813a81baa1
Andrea Avallone <avalloneandrea@gmail.com>
2019-06-28 15:55:01 +0200

    Update station.pipe.spec.ts

--- src/station/station.pipe.spec.ts
+++ src/station/station.pipe.spec.ts
@@ -1,7 +1,27 @@
+import { Test, TestingModule } from '@nestjs/testing';
 import { StationPipe } from './station.pipe';
 
 describe('StationPipe', () => {
-  it('should be defined', () => {
-    expect(new StationPipe()).toBeDefined();
+
+  let stationPipe: StationPipe;
+
+  beforeEach(async () => {
+    const testingModule: TestingModule = await Test.createTestingModule({
+      providers: [StationPipe]
+    }).compile();
+    stationPipe = testingModule.get<StationPipe>(StationPipe);
   });
+
+  it('should be defined', () => {
+    expect(stationPipe).toBeDefined();
+  });
+
+  it('should transform a valid value', () => {
+    expect(stationPipe.transform('2424 - SAPRI|2424-S11721')).toBe('S11721');
+  });
+
+  it('should not transform an invalid value', () => {
+    expect(stationPipe.transform('')).toBeNull();
+  });
+
 });
--- src/station/station.pipe.ts
+++ src/station/station.pipe.ts
@@ -3,10 +3,10 @@ import { Injectable, PipeTransform } from '@nestjs/common';
 @Injectable()
 export class StationPipe implements PipeTransform {
 
-  private regExp: RegExp = /\w+$/;
+  private regExp: RegExp = /(\w+)$/;
 
   transform(value: string): string {
-    return this.regExp.test(value) ? this.regExp.exec(value)[0] : null;
+    return this.regExp.test(value) ? this.regExp.exec(value)[1] : null;
   }
 
 }
