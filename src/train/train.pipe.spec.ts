e0ce81707f519f79953ef9e74f03f8063e369062
Andrea Avallone <avalloneandrea@gmail.com>
2019-06-28 16:13:59 +0200

    Update train.pipe.spec.ts

--- src/train/train.pipe.spec.ts
+++ src/train/train.pipe.spec.ts
@@ -1,7 +1,29 @@
+import { Test, TestingModule } from '@nestjs/testing';
 import { TrainPipe } from './train.pipe';
 
 describe('TrainPipe', () => {
-  it('should be defined', () => {
-    expect(new TrainPipe()).toBeDefined();
+
+  let trainPipe: TrainPipe;
+
+  beforeEach(async () => {
+    const testingModule: TestingModule = await Test.createTestingModule({
+      providers: [TrainPipe]
+    }).compile();
+    trainPipe = testingModule.get<TrainPipe>(TrainPipe);
   });
+
+  it('should be defined', () => {
+    expect(trainPipe).toBeDefined();
+  });
+
+  it('should transform a valid value', () => {
+    expect(trainPipe.transform('2424')).toBe('2424');
+    expect(trainPipe.transform('@treno 2424')).toBe('2424');
+    expect(trainPipe.transform('Remind: @treno 2424.')).toBe('2424');
+  });
+
+  it('should not transform an invalid value', () => {
+    expect(trainPipe.transform('')).toBeNull();
+  });
+
 });
--- src/train/train.pipe.ts
+++ src/train/train.pipe.ts
@@ -3,10 +3,10 @@ import { Injectable, PipeTransform } from '@nestjs/common';
 @Injectable()
 export class TrainPipe implements PipeTransform {
 
-  private regExp: RegExp = /\d+\.?$/;
+  private regExp: RegExp = /(\d+)\.?$/;
 
   transform(value: string): string {
-    return this.regExp.test(value) ? this.regExp.exec(value)[0] : null;
+    return this.regExp.test(value) ? this.regExp.exec(value)[1] : null;
   }
 
 }
