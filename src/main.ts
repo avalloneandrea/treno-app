6a1fe28b994525cebbafbf68b9a2b70f999dff9d
Andrea Avallone <avalloneandrea@gmail.com>
2019-07-13 17:02:15 +0200

    Change listening port

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/', app, document);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
